7911fea6156dcd4ad1f121716bc61e4fa1dc746c
Antonio Di Giulio <antonio.digiulio@winwinit.tech>
2022-04-11 23:12:32 +0200

    First SpringBoot setup

---
+++ burraco-tournaments-tracker-backend/src/main/java/it/digiulio/burracotournamentstrackerbackend/BurracoTournamentsTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package it.digiulio.burracotournamentstrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurracoTournamentsTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurracoTournamentsTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ burraco-tournaments-tracker-backend/src/test/java/it/digiulio/burracotournamentstrackerbackend/BurracoTournamentsTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.digiulio.burracotournamentstrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurracoTournamentsTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
